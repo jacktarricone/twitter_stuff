db912d0e794242779f2c99acd2f9ed92d9b4fe3f
jacktarricone <54866783+jacktarricone@users.noreply.github.com>
2022-01-19 21:12:33 -0800

    last one

--- cr7_script_drafting.R
+++ cr7_script_drafting.R
@@ -1,9 +1,7 @@
 remotes::install_github('ewenme/understatr')
 library(understatr)
 library(worldfootballR)
-library(ggplot2)
-library(dplyr)
-
+library(tidyverse)
 
 # load in MU 2021 team data
 mu_2021 <-get_team_players_stats(team_name = "Manchester United", year = 2021)
@@ -14,12 +12,42 @@ mu_2020 <-get_team_players_stats(team_name = "Manchester United", year = 2020)
 # bind dfs
 mu_stats_raw <-rbind(mu_2021, mu_2020)
 
-# filter for players only playing over 150 mintues
-mu_stats <-filter(mu_stats_raw, time > 150)
+# filter out cr7
+mu_stats_raw_nocr7 <-filter(mu_stats_raw, player_id != "2371")
 
-ggplot(mu_stats, aes(group = year, x=year, y=xG)) + 
+# add new metrics
+mu_stats <-mutate(mu_stats_raw,
+  shots_per_90min = (shots / time) * 90,
+  goals_per_90min = (goals / time) * 90 
+)
+
+# filter for fowards
+mu_forwards <-filter(mu_stats, str_detect(position, 'F'))
+mu_mids <-filter(mu_stats, str_detect(position, 'M'))
+
+# bind good data
+mu_good_cr <-rbind(mu_forwards, mu_mids)
+
+# test plot
+ggplot(mu_good, aes(file = year, x=year, y=goals_per_90min)) + 
   geom_boxplot()
 
+ggplot(mu_good, aes(group = year, x=year, y=shots_per_90min)) + 
+  geom_boxplot()
+
+theme_set(theme_light(11))
+ggplot(mu_good, aes(group = year,  x=year, y=xG)) + 
+  geom_boxplot() +
+  scale_x_continuous(breaks=c(2020,2021))+
+  labs(title = "Manchester United Forward and Midfielder Expected Goals without CR7")
+
+
+ggplot(mu_good_cr, aes(group = year,  x=year, y=xG)) + 
+  geom_boxplot() +
+  scale_x_continuous(breaks=c(2020,2021))+
+  labs(title = "Manchester United Forward and Midfielder Expected Goals with CR7")
+  
+write.csv(mu_good, "/Users/jacktarricone/Desktop/twitter/cr7/mu_player_stats.csv")
 
 
 bruno <-get_player_seasons_stats(1228)
