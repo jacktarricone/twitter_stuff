61728c198b9d04f3b911402a5e93d794d78f179c
jacktarricone <54866783+jacktarricone@users.noreply.github.com>
2022-11-06 18:15:38 -0800

    graphs

--- how_bad_is_cr7_currently?.R
+++ how_bad_is_cr7_currently?.R
@@ -25,32 +25,120 @@ for (i in 1:length(epl_teams$team)){
 players_df <-bind_rows(all_players, .id = "team")
 head(players_df)
 
-
-mu_2022 <-get_team_players_stats(team_name = "Manchester United", year = 2022)
-
-
 # add new metrics
-mu_stats <-mutate(mu_2022,
+players_df <-mutate(players_df,
   shots_per_90min = (shots / time) * 90,
   goals_per_90min = (goals / time) * 90 
 )
 
-# filter for fowards
-mu_forwards <-filter(mu_stats, str_detect(position, 'F'))
-mu_mids <-filter(mu_stats, str_detect(position, 'M'))
+# add shots per goal
+players_df <-mutate(players_df,
+       shots_per_goal = (shots_per_90min / goals_per_90min)
+)
 
-# bind good data
-mu_good <-rbind(mu_forwards, mu_mids)
+# filter for fowards and strikers
+forwards <-filter(players_df, str_detect(position, 'F'))
+#strikers <-filter(players_df, str_detect(position, 'S'))
 
 # filter out cr7
-mu_good_nocr7 <-filter(mu_good, player_id != "2371")
+cr7 <-filter(forwards, player_id == "2371")
+cr7_gp90 <-cr7$goals_per_90min
+
+# quick plot
+hist(forwards$shots_per_90min, breaks = 40)
 
 # test plot
-theme_set(theme_light(11))
+theme_set(theme_classic(12))
 
 # goals per 90
-ggplot(mu_good_nocr7, aes(group = year, x=year, y=goals_per_90min)) + 
-  geom_boxplot()
+ggplot(forwards) + 
+  geom_histogram(aes(goals_per_90min), fill="#69b3a2", color="#e9ecef", alpha=0.9) +
+  geom_vline(xintercept=cr7_gp90, linetype="dashed", color = "red") + 
+  annotate("text",x = .26, y = 20, label = "CR7", color = "red") +
+  labs(title = "EPL 2022 Fowards/Strikers Goals per 90 Minutes",
+       x = "Goals per 90 mintues",
+       y = "Count")
+
+# ggsave(file = "forwards_goals_per_90_2020.png",
+#        width = 6,
+#        height = 4,
+#        dpi = 400)
+
+
+# shots per 90
+cr7_sp90 <-cr7$shots_per_90min
+
+ggplot(forwards) + 
+  geom_histogram(aes(shots_per_90min), fill="darkviolet", color="#e9ecef", alpha=0.9) +
+  geom_vline(xintercept=cr7_sp90, linetype="dashed", color = "red") + 
+  annotate("text",x = 4, y = 18, label = "CR7", color = "red") +
+  labs(title = "EPL 2022 Fowards/Strikers Shots per 90 Minutes",
+       x = "Shots per 90 mintues",
+       y = "Count")
+
+# ggsave(file = "forwards_shots_per_90_2020.png",
+#        width = 6,
+#        height = 4,
+#        dpi = 400)
+
+# shots per goal
+cr7_spg <-cr7$shots_per_goal
+
+ggplot(forwards) + 
+  geom_histogram(aes(shots_per_goal), fill="firebrick", color="#e9ecef", alpha=0.9) +
+  geom_vline(xintercept=cr7_spg, linetype="dashed", color = "black") + 
+  annotate("text", x = 23.6, y = 18, label = "CR7", color = "black") +
+  labs(title = "EPL 2022 Fowards/Strikers Shots per goal",
+       x = "Shots per goal",
+       y = "Count")
+
+ggsave(file = "forwards_shots_per_goal_2020.png",
+       width = 6,
+       height = 4,
+       dpi = 400)
+
+
+
+# above 0 gp90
+forwards_0 <-filter(forwards, goals_per_90min > 0)
+hist(forwards_0$goals_per_90min)
+
+# goals per 90
+ggplot(forwards_0) + 
+  geom_histogram(aes(goals_per_90min), fill="#69b3a2", color="#e9ecef", alpha=0.9) +
+  geom_vline(xintercept=cr7_gp90, linetype="dashed", color = "red") + 
+  annotate("text",x = .26, y = 20, label = "CR7", color = "red") +
+  labs(title = "EPL 2022 Fowards/Strikers Goals per 90 Minutes",
+       x = "Goals per 90 mintues",
+       y = "Count")
+
+ggsave(file = "forwards_goals_per_90_2020.png",
+       width = 6,
+       height = 4,
+       dpi = 400)
+
+# relocate cols
+forwards <-forwards %>% relocate(shots_per_90min)
+forwards <-forwards %>% relocate(goals_per_90min)
+forwards$team <-as.numeric(forwards$team)
+
+# max goals per team
+max_gp90 <-forwards %>% 
+  group_by(team) %>%
+  filter(goals_per_90min == max(goals_per_90min))
+
+# print
+max_gp90$goals_per_90min <-round(max_gp90$goals_per_90min, digits = 2)
+max_gp90$shots_per_90min <-round(max_gp90$shots_per_90min, digits = 2)
+print(max_gp90)
+
+ 
+max_gp90 <-mutate(max_gp90,
+                    shots_per_goal = (shots_per_90min / goals_per_90min)
+)
+
+
+
 
 # shots per 90
 ggplot(mu_good_nocr7, aes(group = year, x=year, y=shots_per_90min)) + 
