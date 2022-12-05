dab79beb5da591467362ee01edf7b2cf8b85df65
jacktarricone <54866783+jacktarricone@users.noreply.github.com>
2022-12-05 09:05:36 -0800

    table

--- wc_pool_robust_data_analyzation.R
+++ wc_pool_robust_data_analyzation.R
@@ -1,49 +1,47 @@
 library(googlesheets4)
 library(tibble)
+library(tidyverse)
+library(data.table)
+
+
+## group sheet
+group_sheet <-read.csv("/Users/jacktarricone/Desktop/twitter/2022 Qatar Bracket Challenge - Group Stage.csv")
+
+# remove undeeded data
+group_v2 <-group_sheet[-c(1,2:35,37,38),-c(1:2)]
+head(group_v2)
+group_points <-transpose(group_v2)
 
 # define url to sheed and read in jsut top section
-url <-"https://docs.google.com/spreadsheets/d/1IQZRgsarCoNi18ZZwgC9SczFyPZZmD0kgNvJZZS7q08/edit#gid=1525044235"
-picks <-as.data.frame(range_read(url, range = "A1:AC17"), row.names = FALSE)
-head(picks)
+url <-"https://docs.google.com/spreadsheets/d/1IQZRgsarCoNi18ZZwgC9SczFyPZZmD0kgNvJZZS7q08/edit#gid=1704876733"
+names_raw <-range_read(url, range = "A1:AB1", sheet = "Knockout Rounds")
+names_v1 <-colnames(names_raw)
+names <-names_v1[-1]
 
-### format for analysis
-group <-c("A","A","B","B","C","C","D","D","E","E","F","F","G","G","H","H")
-place <-c("1st","2nd","1st","2nd","1st","2nd","1st","2nd",
-          "1st","2nd","1st","2nd","1st","2nd","1st","2nd")
+# knockout points
+points_raw <-range_read(url, range = "A24:AB24", sheet = "Knockout Rounds")
+pr_v2 <-colnames(points_raw) # extract col names
+pr_v3 <-substr(pr_v2,1,nchar(pr_v2)-4) # remove name change 
+pr_v4 <-pr_v3[-1]
+knockout_points <-as.numeric(pr_v4)
 
-# group
-picks$Picks <-group
-names(picks)[1] <-"group"
+# make table
+standings <-as.data.frame(cbind(names,group_points$V1,knockout_points))
 
-# place
-picks$'...2' <-place
-names(picks)[2] <-"place"
+# rename cols
+colnames(standings)[1] <-"Participant"
+colnames(standings)[2] <-"Group Points"
+colnames(standings)[3] <-"Knockout Points"
 
-# current results
-results <-c("Netherlands", "Senegal", # A
-            "England", "USA",         # B
-            "NA", "NA",               # C
-            "NA", "NA",               # D
-            "NA", "NA",               # E
-            "NA", "NA",               # F
-            "NA", "NA",               # G
-            "NA", "NA")               # H
+# make int
+standings$`Group Points` <-as.integer(standings$`Group Points`)
+standings$`Knockout Points` <-as.integer(standings$`Knockout Points`)
 
-# add results
-picks <-add_column(picks, results, .after = "place")
+# add total
+standings$Total <-standings$`Group Points`+standings$`Knockout Points`
 
-# make standings table
-Participant <-colnames(picks[4:30])
-Participant 
-
-Points <-rep(NA,27)
-
-# cbind
-standings <-as.data.frame(cbind(Participant, Points), row.names = FALSE)
-standings
-
-
-picks$Reany
-
+# order by total
+ordered <-standings[order(-standings$Total),]
+ordered
 
 
